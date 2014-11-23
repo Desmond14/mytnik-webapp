bb8c84797eecd55be9265809781aef690ae5fd2b
Desmond14 <s.lakomy@gmail.com>
2014-11-23 20:54:18 +0100

    Removed unused views

--- SeeWeb/webint/views.py
+++ SeeWeb/webint/views.py
@@ -24,52 +24,34 @@ def json_encode_decimal(obj):
     raise TypeError(repr(obj) + " is not JSON serializable")
 
 
-def index(request):
-    if request.user.is_authenticated():
-        context = RequestContext(request)
-        size = getNumberOfManifests()
-        pagination = 0
-        pagination = int(math.ceil(size / 20.0))
-        pages = range(pagination)
-        context_dict = {'number_of_pages': pages}
-        return render_to_response('webint/index.html', context_dict, context)
-    else:
-        return HttpResponseRedirect('/webint/not_logged_in')
-
-
 def update_status(request):
-    context = RequestContext(request)
     if request.method == "POST":
         status = ContainerStatus.objects.get(container_id=request.POST['container'])
         new_status_param = request.POST.get('new_status')
         if new_status_param is not None:
             status.status = request.POST['new_status']
-            print "This is update_status"
+            #print "This is update_status"
         else:
             new_assignee = User.objects.get(username=request.POST['new_assignee'])
             print new_assignee.username
             status.assignee = new_assignee
-            print "this is update assignee"
+            #print "this is update assignee"
         status.save()
         return HttpResponse(json.dumps(status.status), 'application/json')
     return HttpResponse(status=404)
 
 
-def manifests_no_ajax(request):
+def manifests(request):
     if request.user.is_authenticated():
         context = RequestContext(request)
-        # manif_list = getManifestsNoAjax()
-        #conta_list = getContainers()
-        #bills_list = getBills()
         context_dict = {}
-        return render_to_response('webint/manifest_no_ajax.html', context_dict, context)
+        return render_to_response('webint/manifests.html', context_dict, context)
     else:
         return HttpResponseRedirect('/webint/not_logged_in')
 
 
 def manifests_datatables(request):
     if request.user.is_authenticated():
-        context = RequestContext(request)
         items_list = getManifestsNoAjax()
         items_list_dict = {}
         items_list_dict.update({'aaData': items_list})
@@ -87,7 +69,6 @@ def containers_datatables(request):
 
 def bills_datatables(request):
     if request.user.is_authenticated():
-        context = RequestContext(request)
         items_list = getBills()
         items_list_dict = {}
         items_list_dict.update({'aaData': items_list})
@@ -105,7 +86,6 @@ def containers_with_status_datatables(request):
 
 def bills_per_cont_datatables(request, containerID):
     if request.user.is_authenticated():
-        context = RequestContext(request)
         items_list = getBillsforCont(containerID)
         items_list_dict = {}
         items_list_dict.update({'aaData': items_list})
@@ -115,7 +95,6 @@ def bills_per_cont_datatables(request, containerID):
 def containers_view(request):
     if request.user.is_authenticated():
         context = RequestContext(request)
-        # containers_list = getContainersWithStatus()
         usernames = User.objects.all().values_list('username', flat=True)
         context_dict = {'users': list(usernames)}
         return render_to_response('webint/containers.html', context_dict, context)
@@ -126,18 +105,8 @@ def containers_view(request):
 def bills_view(request):
     if request.user.is_authenticated():
         context = RequestContext(request)
-        # bill_list = getBills()
         context_dict = {}
         return render_to_response('webint/bills.html', context_dict, context)
-    else:
-        return HttpResponseRedirect('/webint/not_logged_in')
-
-
-def customquerry(request):
-    if request.user.is_authenticated():
-        context = RequestContext(request)
-        context_dict = {}
-        return render_to_response('webint/customquerry.html', context_dict, context)
     else:
         return HttpResponseRedirect('/webint/not_logged_in')
 
@@ -151,32 +120,20 @@ def docs(request):
         return HttpResponseRedirect('/webint/not_logged_in')
 
 
-def mylogin(request):
+def user_login(request):
     context = RequestContext(request)
     if request.method == 'POST':
-        # Gather the username and password provided by the user.
-        # This information is obtained from the login form.
         username = request.POST['username']
         password = request.POST['password']
-        # Use Django's machinery to attempt to see if the username/password
-        # combination is valid - a User object is returned if it is.
         user = authenticate(username=username, password=password)
-        # If we have a User object, the details are correct.
-        # If None (Python's way of representing the absence of a value), no user
-        # with matching credentials was found.
         if user:
-            # Is the account active? It could have been disabled.
             if user.is_active:
-                # If the account is valid and active, we can log the user in.
-                # We'll send the user back to the homepage.
                 print user.first_name
                 login(request, user)
                 return HttpResponseRedirect('/webint/')
             else:
-                # An inactive account was used - no logging in!
                 return HttpResponse("Your account is disabled.")
         else:
-            # Bad login details were provided. So we can't log the user in.
             print "Invalid login details: {0}, {1}".format(username, password)
             return HttpResponse("Invalid login details supplied.")
     else:
@@ -188,49 +145,15 @@ def not_logged_in(request):
     return render_to_response('webint/notloggedin.html', {}, context)
 
 
-def mylogout(request):
+def user_logout(request):
     logout(request)
     return HttpResponseRedirect('/webint/')
 
 
-def test_view(request):
-    context = RequestContext(request)
-    context_dict = {}
-    return render_to_response('webint/test.html', context_dict, context)
-
-
-def test_ajax(request, pagenumber):
-    if request.user.is_authenticated():
-        if request.method == "POST":
-            status = ContainerStatus.objects.get(container_id=request.POST['container'])
-            new_status_param = request.POST.get('new_status')
-            if new_status_param is not None:
-                status.status = request.POST['new_status']
-                print "This is update_status"
-            else:
-                new_assignee = User.objects.get(username=request.POST['new_assignee'])
-                print new_assignee.username
-                status.assignee = new_assignee
-                print "this is update assignee"
-            status.save()
-            return HttpResponse(status=201)
-        context = RequestContext(request)
-        data = getManifests(int(pagenumber))
-        return HttpResponse(json.dumps(data), content_type="application/json")
-
-
 def ajax_bills_per_manifest(request, pagenumber, manifestID):
     if request.user.is_authenticated():
-        context = RequestContext(request)
         data = getBills_per_manifest(int(pagenumber), manifestID)
         return HttpResponse(json.dumps(data), content_type="application/json")
-
-
-def ajax_conts_per_manifest(request, pagenumber, manifestID):
-    if request.user.is_authenticated():
-        context = RequestContext(request)
-        data = getContainers_per_manifest(int(pagenumber), manifestID)
-        return HttpResponse(simplejson.dumps(data, default=json_encode_decimal), content_type="application/json")
 
 
 def bills_per_cont(request, containerID):
@@ -241,49 +164,6 @@ def bills_per_cont(request, containerID):
     else:
         return HttpResponseRedirect('/webint/not_logged_in')
 
-
-def unb_single_manifest_details(request, unbRef):
-    if request.user.is_authenticated():
-        context = RequestContext(request)
-        unbref_id = str(unbRef)
-
-        numberOfContainersPerManifest = getNumberOfContainers(unbref_id)
-        numberOfBillsPerManifest = getNumberOfBills(unbref_id)
-        print numberOfContainersPerManifest
-        print numberOfBillsPerManifest
-        paginationCont = int(math.ceil(numberOfContainersPerManifest / 20.0))
-        paginationBill = int(math.ceil(numberOfBillsPerManifest / 20.0))
-        pagesCont = range(paginationCont)
-        pagesBill = range(paginationBill)
-
-        data = getSingleManifet(unbRef)
-        context_dict = {'data': data, 'pagesBill': pagesBill, 'pagesCont': pagesCont, 'unb': unbRef}
-        return render_to_response('webint/singlemanf.html', context_dict, context)
-    else:
-        return HttpResponseRedirect('/webint/not_logged_in')
-
-
-def single_manifest_details(request, manifestID):
-    if request.user.is_authenticated():
-        context = RequestContext(request)
-        manf_id = str(manifestID)
-
-        numberOfContainersPerManifest = getNumberOfContainers(manf_id)
-        numberOfBillsPerManifest = getNumberOfBills(manf_id)
-        print numberOfContainersPerManifest
-        print numberOfBillsPerManifest
-        paginationCont = 0
-        paginationBill = 0
-        paginationCont = int(math.ceil(numberOfContainersPerManifest / 20.0))
-        paginationBill = int(math.ceil(numberOfBillsPerManifest / 20.0))
-        pagesCont = range(paginationCont)
-        pagesBill = range(paginationBill)
-
-        data = getSingleManifet(manf_id)
-        context_dict = {'data': data, 'pagesBill': pagesBill, 'pagesCont': pagesCont}
-        return render_to_response('webint/singlemanf.html', context_dict, context)
-    else:
-        return HttpResponseRedirect('/webint/not_logged_in')
 
 def alerts(request):
     if request.user.is_authenticated():
